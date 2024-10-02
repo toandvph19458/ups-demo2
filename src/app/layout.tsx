98ea369702ef2296b924d72ada3817c2751a8b96
Duong Van Toan <105204332+toandvph19458@users.noreply.github.com>
2024-10-02 17:10:30 +0700

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 			<body>
 				<ThemeProvider
 					attribute="class"
-					defaultTheme="system"
+					defaultTheme="light"
 					enableSystem
 				>
 					<TheHeader/>
